601251f5c8fc79c9082b96307c84995660756c53
whichwaytogo <vasubhat@gmail.com>
2015-01-27 23:34:26 +0100

    javascript lesson
    Got the javascript,, just the div is positioned wrong

--- initializr/js/main.js
+++ initializr/js/main.js
@@ -1,1 +1,10 @@
+function showMood(){
+	name = document.getElementById('name').value;
+	mood = document.getElementById('mood').value;
 
+	moodString = name+" is "+mood+" today ";
+
+	holder = document.getElementById('holder');
+	
+	holder.innerHTML = moodString;
+}
